528fcf30bc94bf21af8c704e624d4faf660e9df9
rajkumarpawar07 <pawarrajkumar0707@gmail.com>
2025-05-24 23:41:20 +0530

    Hello2

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Hello from {os.getenv('ENV', 'unknown')} environment!"
+    return f"Hello2 from {os.getenv('ENV', 'unknown')} environment!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
